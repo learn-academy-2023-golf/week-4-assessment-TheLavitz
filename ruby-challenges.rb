dc3f8dd09fce98b4d90044b6f5a896e7351a3b17
TheLavitz <lavitz@gmail.com>
2023-10-29 15:33:50 -0700

    Assessment complete

--- ruby-challenges.rb
+++ ruby-challenges.rb
@@ -11,6 +11,23 @@ reposts3 = 221
 # Expected output: '221 is odd'
 
 # Pseudo code:
+# Define a method called even_odd
+# Input: Number
+# Output: String
+# Use if condtional and modulo to determine if a number is odd or even
+# Use string interpolation to return a string containing the input and stating whether it's even or odd
+
+def even_odd num
+    if num % 2 == 0
+        "#{num} is even."
+    else
+        "#{num} is odd."
+    end
+end
+
+p even_odd reposts1
+p even_odd reposts2
+p even_odd reposts3
 
 # -------------------2) Create a method that takes in a string and removes all the vowels from the string. Use the test variables provided. 
 # HINT: Check out this resource: https://ruby-doc.org/core-2.6/String.html#method-i-delete
@@ -23,6 +40,18 @@ beatles_album3 = 'Abbey Road'
 # Expected output: 'bby Rd'
 
 # Pseudo code:
+# Define a method called no_vowels
+# Input: string
+# Output: string
+# Use .delete! to remove the vowels from the string
+
+def no_vowels str
+    str.delete! "aeiou"
+end
+
+p no_vowels beatles_album1
+p no_vowels beatles_album2
+p no_vowels beatles_album3
 
 # -------------------3) Create a method that takes in a string and checks if the string is a palindrome. A palindrome is the same word spelled forward or backward. Use the test variables provided.
 
@@ -34,3 +63,20 @@ palindrome_test_case3 = 'Rotator'
 # Expected output: 'Rotator is a palindrome'
 
 # Pseudo code:
+# Define a method called is_palindrome
+# Input: string
+# Output: string
+# Use if conditional, .downcase, and .reverse to determine if provided string is a palindrome
+# Use string interpolation to return the input and a string stating whether or not the input is palindrome
+
+def is_palindrome str
+    if str.downcase.reverse == str.downcase
+        "#{str} is a palindrome."
+    else
+        "#{str} is not a palindrome."
+    end
+end
+
+p is_palindrome palindrome_test_case1
+p is_palindrome palindrome_test_case2
+p is_palindrome palindrome_test_case3
